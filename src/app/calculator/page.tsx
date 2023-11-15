21b6f9cf459864a054a826eeb265cf59aa79602e
Radoslaw Olejniczak <radoslawolejniczak@codeabra.dev>
2023-11-15 21:45:53 +0100

    input component, adding rows to form

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -1,5 +1,5 @@
-import Link from 'next/link'
 import Container from '@/components/atoms/Container'
+import Calculator from '@/components/molecules/Calculator'
 
 export default function Home() {
   return (
@@ -12,69 +12,7 @@ export default function Home() {
         </Container>
       </section>
       <section>
-        <Container>
-          <div className="flex -mx-2 mb-2">
-            <div className="flex-1 mx-2">Food</div>
-            <div className="flex-none w-24 mx-2">Serving</div>
-            <div className="flex-none w-24 mx-2">Carbs</div>
-            <div className="flex-none w-24 mx-2">Fiber</div>
-            <div className="flex-none w-24 mx-2">GI</div>
-            <div className="flex-none w-12 mx-2"></div>
-          </div>
-          <ul>
-            <li className="flex -mx-2">
-              <div className="flex-1 mx-2">
-                <input
-                  className="shadow appearance-none border rounded w-full h-10 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  id="username"
-                  type="text"
-                  value=""
-                  defaultValue=""
-                />
-              </div>
-              <div className="flex-none w-24 mx-2">
-                <input
-                  className="shadow appearance-none border rounded w-full h-10 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  type="number"
-                  value=""
-                  defaultValue=""
-                />
-              </div>
-              <div className="flex-none w-24 mx-2">
-                <input
-                  className="shadow appearance-none border rounded w-full h-10 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  type="number"
-                  value=""
-                  defaultValue=""
-                />
-              </div>
-              <div className="flex-none w-24 mx-2">
-                <input
-                  className="shadow appearance-none border rounded w-full h-10 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  type="number"
-                  value=""
-                  defaultValue=""
-                />
-              </div>
-              <div className="flex-none w-24 mx-2">
-                <input
-                  className="shadow appearance-none border rounded w-full h-10 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                  type="number"
-                  value=""
-                  defaultValue=""
-                />
-              </div>
-              <div className="flex-none w-12 mx-2">
-                <button
-                  className="shadow appearance-none border rounded w-full h-10 text-lg font-semibold bg-green-600 text-white focus:outline-none focus:shadow-outline"
-                  type="button"
-                >
-                  +
-                </button>
-              </div>
-            </li>
-          </ul>
-        </Container>
+        <Calculator />
       </section>
     </>
   )
